527a9b30f18395108fbbfcff80a9f662800995a3
yangshuai <yangshuai@yangshuaideMacBook-Pro.local>
2017-08-30 15:30:11 +0800

    确认屏幕分辨率

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,7 @@
  window.onload = function () {
      //判断是否来自登录界面跳转
      if (-1 !== location.search.substr(1).indexOf("from=login")) {core.close("login");}
+     core.byId("logout").onclick = function () {
+         //退出处理ßß
+     }
  }
