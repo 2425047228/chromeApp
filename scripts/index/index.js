3cf8c622fa5816cbbf5c7c79f847bab34993e019
yangshuaiWithCompany <go.xlv@qq.com>
2017-09-02 11:34:15 +0800

    修复小数分割

--- scripts/index/index.js
+++ scripts/index/index.js
@@ -3,15 +3,12 @@ window.onload = function () {
     var paramObj = core.paramToObject(location.search.substr(1));
     //传递订单总数
     if (typeof paramObj.order !== "undefined") core.byId("order").innerText = paramObj.order;
-
     //传递金额总价
     if (typeof paramObj.amount !== "undefined") {
-        paramObj.amount = "100.54";
-        var int = parseInt(paramObj.amount);
-        var float = paramObj.amount.split('.')[1];
-        if (int > 0) core.byId("int").innerText = int;
-        if (float > 0) core.byId("float").innerText = '.' + float;
-
+        paramObj.amount = paramObj.amount.toString();
+        var numberArr =  paramObj.amount.split('.');
+        if (numberArr[0] > 0) core.byId("int").innerText = numberArr[0];
+        if (numberArr.length > 1 && numberArr[1] > 0) core.byId("float").innerText = '.' + numberArr[1];
     }
 
 }
